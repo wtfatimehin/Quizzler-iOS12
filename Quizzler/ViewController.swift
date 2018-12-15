5acaee6827ed93d199679f4bad60313cd1bf7e14
Willie Fatimehin <willie.fatimehin@gmail.com>
2018-12-15 17:43:25 -0500

    added increments to questions answer

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Place your instance variables here
+    let allQuestions = QuestionBank()
+    var pickedAnswer: Bool = false
+    var questionNumber: Int = 0
     
     
     @IBOutlet weak var questionLabel: UILabel!
@@ -21,11 +23,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestions = allQuestions.list[0]
+        questionLabel.text = firstQuestions.questionText
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
+        
+        questionNumber += 1
+        
+        questionLabel.text = allQuestions.list[questionNumber].questionText
+        
+        
     }
     
     
@@ -40,12 +57,23 @@ class ViewController: UIViewController {
     
     
     func checkAnswer() {
+        let correctAnswer = allQuestions.list[questionNumber].answer
         
+        if correctAnswer == pickedAnswer {
+            print("You got it")
+        }
+        else {
+            print("wrong!")
+        }
+        
+        
+        
+       
     }
     
     
     func startOver() {
-       
+   
     }
     
 
